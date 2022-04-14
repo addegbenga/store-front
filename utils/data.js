d0eb02ec31963a5d2c34b7504398147e56b52f29
Adeyemi <addegbenga@gmail.com>
2022-04-14 13:54:37 +0100

    first commit

---
+++ utils/data.js
@@ -0,0 +1,87 @@
+export const dataCategory = [
+  {
+    img: "/1available services.jpeg",
+    title: "Available services",
+    id: 1,
+  },
+  {
+    img: "/2news.jpeg",
+    title: "News & Annoucement",
+    id: 2,
+  },
+  {
+    img: "/3marketnews.jpeg",
+    title: "Directories",
+    id: 2,
+  },
+  {
+    img: "/4directories.jpeg",
+    title: "Open For Comment",
+    id: 2,
+  },
+  {
+    img: "/5reports.jpeg",
+    title: "Open For Comment",
+    id: 2,
+  },
+  {
+    img: "/6comments.jpeg",
+    title: "Open For Comment",
+    id: 2,
+  },
+];
+
+export const dataProgram = [
+  {
+    img: "/TM.jpeg",
+    title: "Science & technology",
+    id: 2,
+  },
+  {
+    img: "/CP2.jpeg",
+    title: "Commodity Procurement",
+    id: 1,
+  },
+  {
+    img: "/C&T.jpeg",
+    title: "Cotton & Tobacco",
+    id: 2,
+  },
+  {
+    img: "/TM.jpeg",
+    title: "Transportation & marketing",
+    id: 2,
+  },
+
+  {
+    img: "/FTPP.jpeg",
+    title: "fair trade practices",
+    id: 2,
+  },
+  {
+    img: "/FGIS.jpeg",
+    title: "federal grain inspection",
+    id: 2,
+  },
+  {
+    img: "/homepage_programs_lp.png",
+    title: "Livestock & poultry",
+    id: 2,
+  },
+  {
+    img: "/NOP.jpeg",
+    title: "National organic program",
+    id: 2,
+  },
+  {
+    img: "/dairy.jpeg",
+    title: "Diary",
+    id: 2,
+  },
+
+  {
+    img: "/S&T.jpeg",
+    title: "Science & technology",
+    id: 2,
+  },
+];
