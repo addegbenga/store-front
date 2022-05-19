833e55bccce54a9e57e66bc6654c580b5272cb3a
Adeyemi <addegbenga@gmail.com>
2022-05-20 00:13:30 +0100

    feat: added ui element to cart modal

--- components/commons/Cart/CartModal.tsx
+++ components/commons/Cart/CartModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
+import Image from 'next/image';
 
 const CartModal: React.FC<any> = () => {
   let [isOpen, setIsOpen] = useState(true);
@@ -51,8 +52,69 @@ const CartModal: React.FC<any> = () => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full bg-white w-3/12  h-screen max-w-md transform overflow-hidden p-6 text-left align-middle shadow-xl transition-all">
-                  <div className="mt-2">
-                    <h1>Hello world</h1>
+                  <div className="my-2">
+                    <h1 className="text-2xl">Shopping Cart</h1>
+                    <p className="text-sm my-4">
+                      Congratulations you have got free shipping
+                    </p>
+                  </div>
+                  <div className="bg-gray-100 py-5 px-3 flex items-center gap-4 h-fit  bg-opacity-70">
+                    <div className="h-36 w-32  bg-white">
+                      <Image
+                        src="/assets/product1.webp"
+                        alt="cart_img"
+                        width="100%"
+                        height="100%"
+                        layout="responsive"
+                      />
+                    </div>
+                    <div>
+                      <span className="text-black text-opacity-60 text-sm">
+                        M
+                      </span>
+                      <p className="text-sm">
+                        Apparel Clothing <br /> Menswear White...
+                      </p>
+                      <span>3 x $350.00</span>
+                      <div className="flex bg-white p-1.5 my-2 rounded-full justify-between">
+                        <button>-</button>
+                        <span>3</span>
+                        <button>+</button>
+                      </div>
+                    </div>
+                  </div>
+                  {/* discount section */}
+                  <div className="mt-10 ">
+                    <div className="flex justify-between">
+                      <p>Original</p>
+                      <span className="font-bold text-sm">$1,050.00</span>
+                    </div>
+                    <div className="flex justify-between">
+                      <p>Discounted</p>
+                      <span className="font-bold text-sm">$0.00</span>
+                    </div>
+                    <div className="border-t flex justify-between my-3  pt-3">
+                      <p className="font-bold">Total</p>
+                      <span className="font-bold text-sm">$1,050.00</span>
+                    </div>
+                  </div>
+                  {/* discount section */}
+                  <div className="my-8">
+                    <div className="flex gap-2 items-center">
+                      <input type="checkbox" />
+                      <p className="text-sm">
+                        I agree with the{' '}
+                        <span className="text-green-700 font-semibold">
+                          terms and conditions
+                        </span>
+                      </p>
+                    </div>
+                    <div className="flex flex-col my-5 gap-2">
+                      <button className="border p-2">VIEW CART</button>
+                      <button className="bg-green-700 p-2.5 text-sm text-white">
+                        CHECK OUT
+                      </button>
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
