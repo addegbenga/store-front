ef87bbf59e81c390e3888d63726ec6f3c72f4641
Adeyemi <addegbenga@gmail.com>
2022-05-20 15:15:00 +0100

    feat: added checkout section

--- components/commons/Checkout/CheckOut.tsx
+++ components/commons/Checkout/CheckOut.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 interface ICheckOutProps {}
 const CheckOut: React.FC<ICheckOutProps> = () => {
   return (
     <div className="">
       <div className="grid grid-cols-2 ">
         {/* shipping info section */}
-        <div className="py-10 pl-32 pr-10 bg-white ">
+        <div className="w-full py-10 pl-32 pr-10 bg-white ">
           <h1 className="text-2xl font-semibold">KARA</h1>
           <div className="flex gap-2 my-2 text-sm">
             <p>Information</p>
@@ -19,7 +20,8 @@ const CheckOut: React.FC<ICheckOutProps> = () => {
             <div className="flex justify-between my-3 mt-5">
               <h1>Contact Information</h1>
               <p className="text-sm">
-                Already have an account ? <span>Log in</span>
+                Already have an account ?{' '}
+                <span className="text-blue-500">Log in</span>
               </p>
             </div>
             <form>
@@ -35,11 +37,11 @@ const CheckOut: React.FC<ICheckOutProps> = () => {
               <div className="grid gap-2 my-8">
                 <h1 className="mb-3">Shipping Address</h1>
                 <select
-                  className="w-full p-3 text-xs border rounded"
+                  className="w-full p-3 text-xs bg-white border rounded"
                   name="cars"
                   id="cars"
                 >
-                  <option className="text-xs" value="" disabled selected>
+                  <option className="text-sm " value="" disabled selected>
                     select country/region
                   </option>
                   <option value="volvo">Nigeria</option>
@@ -102,8 +104,63 @@ const CheckOut: React.FC<ICheckOutProps> = () => {
           </div>
         </div>
         {/* end of shipping info section */}
-        <div className="h-screen py-10 bg-gray-100">
-          <h1>phase 2</h1>
+        <div className="h-screen py-10 bg-gray-100 ">
+          <div className="flex flex-col max-w-lg gap-2 mx-auto">
+            <div className="flex items-center justify-between h-fit">
+              <div className="flex items-center gap-4">
+                <div className="bg-white border rounded w-[6rem] h-[6rem">
+                  <Image
+                    src="/assets/product1.webp"
+                    alt="cart_img"
+                    width="100%"
+                    height="100%"
+                    layout="responsive"
+                  />
+                </div>
+                <div>
+                  <p className="text-sm">Apparel Clothing</p>
+                  <span className="text-sm text-black text-opacity-60">M</span>
+                </div>
+              </div>
+              <span>$350.00</span>
+            </div>
+            <div className="flex items-center justify-between h-fit">
+              <div className="flex items-center gap-4">
+                <div className="bg-white border rounded w-[6rem] h-[6rem">
+                  <Image
+                    src="/assets/product1.webp"
+                    alt="cart_img"
+                    width="100%"
+                    height="100%"
+                    layout="responsive"
+                  />
+                </div>
+                <div>
+                  <p className="text-sm">Apparel Clothing</p>
+                  <span className="text-sm text-black text-opacity-60">M</span>
+                </div>
+              </div>
+              <span>$350.00</span>
+            </div>
+
+            {/* discount section */}
+            <div className="grid gap-2 mt-4 ">
+              <div className="flex items-center justify-between pt-3 border-t">
+                <p>Subtotal</p>
+                <span className="text-sm font-medium">$1,050.00</span>
+              </div>
+              <div className="flex items-center justify-between">
+                <p>Shipping</p>
+                <span className="text-sm ">Calculated at next step</span>
+              </div>
+              <div className="flex items-center justify-between pt-3 my-3 border-t">
+                <p className="font-medium">Total</p>
+                <span className="flex items-center gap-2 text-xl font-medium ">
+                  <span className="text-xs text-gray-400">USD</span>$1,050.00
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
